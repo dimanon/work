b49527be695ecc8262619880e69f8d8bfe1a1945
dimanon <borodulind@yandex.ru>
2014-01-08 21:43:26 +0200

    rename vars

--- my_script.py
+++ my_script.py
@@ -4,13 +4,13 @@ import sys
 
 
 def tir(mega_ball, dropd):
-    out_mega_ball = {number: number == mega_ball for number in range(10)}
-    out_balls = {number: number in dropd for number in range(1, 43)}
-    return out_mega_ball, out_balls
+    mega_ball = {number: number == mega_ball for number in range(10)}
+    six_of_42 = {number: number in dropd for number in range(1, 43)}
+    return mega_ball, six_of_42
 
 
-def main(in_balls):
-    print(tir(int(in_balls[0]), [int(x) for x in in_balls[1:]]))
+def main(balls):
+    print(tir(int(balls.pop(0)), [int(ball) for ball in balls]))
 
 if __name__ == '__main__' and len(sys.argv) > 1:
     main(sys.argv[1:])
